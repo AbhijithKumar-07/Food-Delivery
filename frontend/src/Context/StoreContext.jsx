29454942a0c19ced3e0d9283ae82c479898a9694
Abhijith Kumar <155456747+AbhijithKumar-07@users.noreply.github.com>
2025-02-21 00:40:48 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -9,7 +9,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = ({ children }) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-vewv.onrender.com";
   const [token,setToken] = useState("");
   const [food_list,setFoodlist] = useState([]);
 
